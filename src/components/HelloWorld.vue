dcb05b8b0e879566617300036f359c063f36953a
TanveerHassan333 <tanveerhassan4445@gmail.com>
2023-09-18 23:53:14 +0500

    this is initial vuetify commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,56 @@
+<template>
+ <VRow>
+  <VCol cols="12" md="6" lg="3">
+    <VCard>
+      <v-card-text>
+        <VCardTitle>
+        This is card
+        </VCardTitle>
+        <VCardText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius reprehenderit itaque mollitia quos distinctio!
+        </VCardText>
+      </v-card-text>
+    </VCard>
+  </VCol>
+  <VCol cols="12" md="6" lg="3">
+    <VCard>
+      <v-card-text>
+        <VCardTitle>
+        This is card
+        </VCardTitle>
+        <VCardText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius reprehenderit itaque mollitia quos distinctio!
+        </VCardText>
+      </v-card-text>
+    </VCard>
+  </VCol>
+  <VCol cols="12" md="6" lg="3">
+    <VCard>
+      <v-card-text>
+        <VCardTitle>
+        This is card
+        </VCardTitle>
+        <VCardText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius reprehenderit itaque mollitia quos distinctio!
+        </VCardText>
+      </v-card-text>
+    </VCard>
+  </VCol>
+  <VCol cols="12" md="6" lg="3">
+    <VCard>
+      <v-card-text>
+        <VCardTitle>
+        This is card
+        </VCardTitle>
+        <VCardText>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius reprehenderit itaque mollitia quos distinctio!
+        </VCardText>
+      </v-card-text>
+    </VCard>
+  </VCol>
+ </VRow>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ src/layouts/default/DefaultPreview.vue
@@ -0,0 +1,7 @@
+<template>
+  sdjsakjd
+</template>
+
+<script setup>
+
+</script>
